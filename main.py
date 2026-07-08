f09e79ccd60b1628728d003298fae2b76e1fc425
Luis <soycatorce.developer@gmail.com>
2026-07-08 13:24:50 -0600

    create interface in terminal

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from elements import machine as machine
 
-print("SIMULADOR DE LA MAQUINA ENIGMA")
-mensaje = input("Ingrese el mensaje a cifrar: ")
+indicador = True
+while indicador:
+    print("\n--------------------------------------------------------------\n")
+    print("SIMULADOR DE LA MAQUINA ENIGMA")
+    mensaje = input("Ingrese el mensaje a cifrar: ")
 
-print("Mensaje original: ", mensaje)
+    print("Mensaje original: ", mensaje)
 
-mensaje_cifrado = machine.cifrar_mensaje(mensaje)
-print("Mensaje cifrado: ", mensaje_cifrado)
+    mensaje_cifrado = machine.cifrar_mensaje(mensaje)
+    print("Mensaje cifrado: ", mensaje_cifrado)
+    
+    continuar = input("Para terminar el programa presione q, para continuar presione cualquier tecla: ")
+    if continuar.lower() == "q": indicador = False
+    else: indicador = True
