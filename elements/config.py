de9ac7be42bebb32a8caa5b21304a2cf1c446c9b
Luis <soycatorce.developer@gmail.com>
2026-07-09 20:10:26 -0600

    modificando codigo de la clase config

--- elements/config.py
+++ elements/config.py
@@ -1,30 +1,33 @@
 from . import cableado
 
 class config:
-    def __init__(self, rotor1:str, rotor2:str, rotor3:str, contraseña:str):
+    def __init__(self, rotor1:str, rotor2:str, rotor3:str, semilla:int):
         self.rotor1 = rotor1
         self.rotor2 = rotor2
         self.rotor3 = rotor3
-        self.contraseña = contraseña
-    
-    def get_semilla(self, semilla:int):
         self.semilla = semilla
     
-    def semillas_rotores(self, semilla:int, rotor1:str, rotor2:str, rotor3:str):
-        semilla_rotor1 = cableado.semilla_rotores(semilla, rotor1)
-        semilla_rotor2 = cableado.semilla_rotores(semilla, rotor2)
-        semilla_rotor3 = cableado.semilla_rotores(semilla, rotor3)
+    def semillas_rotores(self):
+        semilla_rotor1 = cableado.semilla_rotores(self.semilla, self.rotor1)
+        semilla_rotor2 = cableado.semilla_rotores(self.semilla, self.rotor2)
+        semilla_rotor3 = cableado.semilla_rotores(self.semilla, self.rotor3)
         
         return semilla_rotor1, semilla_rotor2, semilla_rotor3
     
-    def posiciones_i_rotores(self, semilla_rotor1:int, semilla_rotor2:int, semilla_rotor3:int):
+    def posiciones_i_rotores(self):
+        
+        semilla_rotor1, semilla_rotor2, semilla_rotor3 = self.semillas_rotores()
+        
         posicion_rotor1 = cableado.pos_i_rotores(semilla_rotor1)
         posicion_rotor2 = cableado.pos_i_rotores(semilla_rotor2)
         posicion_rotor3 = cableado.pos_i_rotores(semilla_rotor3)
         
         return posicion_rotor1, posicion_rotor2, posicion_rotor3
         
-    def cableado_rotores(self, semilla_rotor1:int, semilla_rotor2:int, semilla_rotor3:int):
+    def cableado_rotores(self):
+        
+        semilla_rotor1, semilla_rotor2, semilla_rotor3 = self.semillas_rotores()
+        
         cableado_rotor1 = cableado.crearCableado(semilla_rotor1)
         cableado_rotor2 = cableado.crearCableado(semilla_rotor2)
         cableado_rotor3 = cableado.crearCableado(semilla_rotor3)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from elements import machine, cableado
+from elements import machine, cableado, config
 
 
 indicador = True
@@ -20,17 +20,10 @@ while indicador:
             rotor3 = input("Nombre de tu rotor 3: ")
             
             semilla = cableado.crearSemilla(contraseña)
-            semilla_rotor1 = cableado.semilla_rotores(semilla, rotor1)
-            semilla_rotor2 = cableado.semilla_rotores(semilla, rotor2)
-            semilla_rotor3 = cableado.semilla_rotores(semilla, rotor3)
+            config = config.config(rotor1, rotor2, rotor3, semilla)
             
-            cableado_rotor1 = cableado.crearCableado(semilla_rotor1)
-            cableado_rotor2 = cableado.crearCableado(semilla_rotor2)
-            cableado_rotor3 = cableado.crearCableado(semilla_rotor3)
+            semilla_rotor1, semilla_rotor2, semilla_rotor3 = config.semillas_rotores()
             
-            posicion_rotor1 = cableado.pos_i_rotores(semilla_rotor1)
-            posicion_rotor2 = cableado.pos_i_rotores(semilla_rotor2)
-            posicion_rotor3 = cableado.pos_i_rotores(semilla_rotor3)
                 
             while True:
                 if indicador == False: break
