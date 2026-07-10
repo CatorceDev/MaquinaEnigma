cbb626c3858b6eab258c32e7228e12b1ddb0f902
Luis <soycatorce.developer@gmail.com>
2026-07-09 18:53:49 -0600

    creando funcion que genera el cableado de los rotores en base a la semilla del rotor

--- elements/cableado.py
+++ elements/cableado.py
@@ -1,16 +1,9 @@
 from sys import maxunicode
-import secrets
 from hashlib import sha256
+from random import seed, shuffle
 
 N = maxunicode + 1
 contraseña = "soycatorcc"
-
-def crearCableado(mensaje: str) -> str:
-    #* Esta funcion creará un cableado basado en una f(x) que 
-    
-    
-    
-    letra_cifrada = 0
 
 def crearSemilla(contraseña) -> int:
     #* Esta funcion generará una semilla pseudoaleatoria a partir de la contraseña para la conversación que se usará para crear el cableado de los rotores, el orden de los rotores, su posicion inicial.
@@ -31,5 +24,16 @@ def semilla_rotores(semilla:int, nombre_rotor:str) -> int:
     return semilla_rotor
 
 def pos_i_rotores(semilla_rotor):
+    #* Esta funcion genera la posicion inicial desde donde partira cada rotor a la hora de cifrar a partir de la semilla del rotor
+    
     posicion_rotor = semilla_rotor % N
     return posicion_rotor
+
+
+def crearCableado(semilla_rotor) -> int:
+    #* Esta funcion creará un cableado basado en la semilla del rotor de tal manera que cada rotor tendra un cableado distinto
+    
+    seed(semilla_rotor)
+    cableado = list(range(N))
+    shuffle(cableado)
+    
